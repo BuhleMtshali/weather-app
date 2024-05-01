67de1eaaf16c189025e6aa11157f2d3fe1c4f51f
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-05-01 03:39:37 +0200

    Updated the javascript file

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+function showTempereture(response) {
+  let temperetureElement = document.querySelector("#current-temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperetureElement.innerHTML = response.data.temperature.current;
+
+  temperetureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "56o3d075ed13bd29afc9ftf3ccaa04e3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(showTempereture);
+}
+
 function showCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
